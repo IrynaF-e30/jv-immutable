77bf2d644c1f57f0b4b596b36cc0ef25d871cda3
Iryna Finenko <irynafinenko@gmail.com>
2025-08-19 21:31:00 +0300

    make final class

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -5,11 +5,28 @@ import java.util.List;
 /**
  * Make this class immutable. See requirements in task description.
  */
-public class Car {
-    private int year;
-    private String color;
-    private List<Wheel> wheels;
-    private Engine engine;
+public final class Car {
+    private final int year;
+    private final String color;
+    private final List<Wheel> wheels;
+    private final Engine engine;
+
+    public String changeColor(String newColor) {
+        this.color = newColor;
+
+        public String getColor() {
+            return color;
+        }
+
+
+    }
+
+    public Car(int year, String color, List<Wheel> wheels, Engine engine) {
+        this.year = year;
+        this.color = color;
+        this.wheels = wheels;
+        this.engine = engine;
+    }
 
     //implement this class
 
