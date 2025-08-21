efc5cceb1e1f722cdeae3c978e2ae9b2eddba372
Iryna Finenko <irynafinenko@gmail.com>
2025-08-21 13:07:03 +0300

    fixed problems

--- src/main/java/core/basesyntax/Car.java
+++ src/main/java/core/basesyntax/Car.java
@@ -1,42 +1,84 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-/**
- * Make this class immutable. See requirements in task description.
- */
-public final class Car {
-    private final int year;
-    private final String color;
-    private final List<Wheel> wheels;
-    private final Engine engine;
-
-    public String changeColor(String newColor) {
-        this.color = newColor;
-
-        public String getColor() {
-            return color;
-        }
-
-
-    }
-
+public record Car(int year, String color, List<Wheel> wheels, Engine engine) {
     public Car(int year, String color, List<Wheel> wheels, Engine engine) {
         this.year = year;
         this.color = color;
-        this.wheels = wheels;
-        this.engine = engine;
+
+        // Deep copy коліс
+        List<Wheel> copy = new ArrayList<>();
+        for (Wheel w : wheels) {
+            copy.add(new Wheel(w.getRadius()));
+        }
+        this.wheels = copy;
+
+        // Deep copy двигуна
+        this.engine = engine == null ? null : new Engine(engine.getHorsePower(), engine.getManufacturer());
     }
 
-    //implement this class
+    // Повертаємо mutable копію списку, щоб зовнішні add/remove не впливали на Car
+    @Override
+    public List<Wheel> wheels() {
+        List<Wheel> cloned = new ArrayList<>();
+        for (Wheel w : wheels) {
+            cloned.add(new Wheel(w.getRadius()));
+        }
+        return cloned;
+    }
+
+    @Override
+    public Engine engine() {
+        return engine == null ? null : new Engine(engine.getHorsePower(), engine.getManufacturer());
+    }
+
+    public Car changeColor(String newColor) {
+        return new Car(year, newColor, wheels, engine);
+    }
+
+    public Car changeEngine(Engine newEngine) {
+        return new Car(year, color, wheels, newEngine);
+    }
+
+    public Car addWheel(Wheel newWheel) {
+        List<Wheel> newWheels = new ArrayList<>();
+        for (Wheel w : wheels) {
+            newWheels.add(new Wheel(w.getRadius()));
+        }
+        newWheels.add(new Wheel(newWheel.getRadius()));
+        return new Car(year, color, newWheels, engine);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car car)) return false;
+        return year == car.year &&
+                Objects.equals(color, car.color) &&
+                Objects.equals(wheels, car.wheels) &&
+                Objects.equals(engine, car.engine);
+    }
 
     @Override
     public String toString() {
-        return "Car{"
-            + "year=" + year
-            + ", color='" + color + '\''
-            + ", wheels=" + wheels
-            + ", engine=" + engine
-            + '}';
+        return "Car{" +
+                "year=" + year +
+                ", color='" + color + '\'' +
+                ", wheels=" + wheels +
+                ", engine=" + engine +
+                '}';
     }
 }
+
+
+
+
+
+
+
+
+
+
--- src/main/java/core/basesyntax/Engine.java
+++ src/main/java/core/basesyntax/Engine.java
@@ -1,16 +1,65 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class Engine {
     private int horsePower;
     private String manufacturer;
 
-    //implement this class
+    public Engine(int horsePower, String manufacturer) {
+        this.horsePower = horsePower;
+        this.manufacturer = manufacturer;
+    }
+
+    // Копіювальний конструктор
+    public Engine(Engine other) {
+        this.horsePower = other.horsePower;
+        this.manufacturer = other.manufacturer;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Engine clone() {
+        return new Engine(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Engine engine)) return false;
+        return horsePower == engine.horsePower &&
+                Objects.equals(manufacturer, engine.manufacturer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(horsePower, manufacturer);
+    }
 
     @Override
     public String toString() {
-        return "Engine{"
-            + "horsePower=" + horsePower
-            + ", manufacturer='" + manufacturer + '\''
-            + '}';
+        return "Engine{" +
+                "horsePower=" + horsePower +
+                ", manufacturer='" + manufacturer + '\'' +
+                '}';
     }
 }
+
+
+
+
+
